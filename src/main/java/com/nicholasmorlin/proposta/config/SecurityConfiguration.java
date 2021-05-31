2ce485f23b174f30b3192dde8c7126167092d028
Nicholas Morlin <nicholas.morlin@zup.com.br>
2021-05-31 14:10:00 -0300

    Adicionando a parte de seguranca e integracao com keycloak

---
+++ src/main/java/com/nicholasmorlin/proposta/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.nicholasmorlin.proposta.config;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        System.out.println("Entrou aqui");
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/propostas/**").hasAnyAuthority("SCOPE_orange-talents-scope:write")
+                .antMatchers(HttpMethod.GET, "/api/propostas/**").hasAnyAuthority("SCOPE_orange-talents-scope:read")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
